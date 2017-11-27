8e28eb0a77e6425333c583c09a5806b939e6f390
Mickael Zhang <mickael.zhg@gmail.com>
2017-11-27 14:07:59 +0100

    Initialize Devise and setup db config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "airbnb_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'hetic-g2-01.herokuapp.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
