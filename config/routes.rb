bdfaed9b9a8d86e10496a1b4ece88d73ca1efd00
HugoQuincey <hugo.quincey@hetic.net>
2017-11-27 14:59:58 +0100

    (auth): add signin, login & logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'auth',
+                     path_name: {
+                       sign_in: 'login',
+                       sign_out: 'logout',
+                       edit: 'profile'
+                     }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
