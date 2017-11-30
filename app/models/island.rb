47afd13364bab927feaf9a7ff1da3247835e5162
Mickael Zhang <mickael.zhg@gmail.com>
2017-11-30 16:26:43 +0100

    Add user link in Island model

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,4 +1,6 @@
 class Island < ApplicationRecord
   validates :title, presence: true, length: {maximum: 65}
   validates :description, presence: true, length: {maximum: 300}
+
+  belongs_to :user
 end
