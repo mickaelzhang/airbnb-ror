784cf967e33594851aa99d948bae497a2abe8bf3
Mickael Zhang <mickael.zhg@gmail.com>
2017-12-05 11:45:57 +0100

    Fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130164727) do
+ActiveRecord::Schema.define(version: 20171205103610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(version: 20171130164727) do
   create_table "islands", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_islands_on_user_id"
   end
 
