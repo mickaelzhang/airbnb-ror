b851c4e68fe10e4b28e79cc629ae5ecc0aa2eba9
Mickael Zhang <mickael.zhg@gmail.com>
2017-12-12 17:02:11 +0100

    Add rating model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207142355) do
+ActiveRecord::Schema.define(version: 20171212155453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 20171207142355) do
     t.integer "price_by_night"
     t.string "image"
     t.index ["user_id"], name: "index_islands_on_user_id"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
