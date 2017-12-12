60198ec565263202441044e0702d4e3912bf5d72
Mickael Zhang <mickael.zhg@gmail.com>
2017-12-12 14:50:59 +0100

    Add price and duration of booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,12 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :island
+
+  def duration
+    (self.end_date - self.start_date).to_i + 1
+  end
+
+  def price
+    self.island.price_by_night * self.duration
+  end
 end
