01689e34adb6e75abcb57969e8f7c156172f9410
Mickael Zhang <mickael.zhg@gmail.com>
2017-12-12 16:48:21 +0100

    Diffentiate old and new booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,4 +9,22 @@ class Booking < ApplicationRecord
   def price
     self.island.price_by_night * self.duration
   end
+
+  # Check if an instance is done
+  def is_done(is_done = true)
+    if is_done
+      return self.where('end_date < ?', Date.today)
+    else
+      return self.where('? <= end_date', Date.today)
+    end
+  end
+
+  # Check if list of bookings are done
+  def self.are_done(are_done = true)
+    if are_done
+      return where('end_date < ?', Date.today)
+    else
+      return where('? <= end_date', Date.today)
+    end
+  end
 end
