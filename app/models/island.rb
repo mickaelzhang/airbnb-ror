0c1f07d43c605d8e317cec6d5aa206beadab9cf8
HugoQuincey <hugo.quincey@hetic.net>
2017-12-14 22:41:29 +0100

    [search]: add case-insensitive

--- app/models/island.rb
+++ app/models/island.rb
@@ -29,7 +29,7 @@ class Island < ApplicationRecord
 
   def self.search(search)
     if search
-      where('title LIKE ?', "%#{search}%")
+      where('lower(title) LIKE ?', "%#{search}%")
     else
       find(:all)
     end
